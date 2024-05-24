f4cc34c8f6e3dc13d5a2d4e5a88fa09b7fc3f205
imposible normalito <imposiblenormalito@gmail.com>
2024-05-24 12:05:31 +0100

    frame structure

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Cplus-portfolio
+
+Used to learn C++ language following tutorials.
+
+[Snake game](https://youtu.be/E_-lMZDi7Uw?si=iVPG_y1gGLoyCGIr)
---
+++ snake.h
@@ -0,0 +1,14 @@
+//
+// Created by impos on 24/05/2024.
+//
+
+#ifndef CPLUS_PORTFOLIO_SNAKE_H
+#define CPLUS_PORTFOLIO_SNAKE_H
+
+
+class snake {
+
+};
+
+
+#endif //CPLUS_PORTFOLIO_SNAKE_H
